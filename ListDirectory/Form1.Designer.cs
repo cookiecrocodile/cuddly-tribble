6b932074b75f72183d109398ed08d00070622a60
Karin Jonsson <jonsson.karin.ae@gmail.com>
2015-12-11 17:15:09 +0100

    initial commit

---
+++ ListDirectory/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace ListDirectory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxDrives = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBoxDirectories = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBoxFiles = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxPreview = new System.Windows.Forms.TextBox();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.buttonDirectoryUp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBoxDrives
+            // 
+            this.comboBoxDrives.FormattingEnabled = true;
+            this.comboBoxDrives.Location = new System.Drawing.Point(25, 105);
+            this.comboBoxDrives.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBoxDrives.Name = "comboBoxDrives";
+            this.comboBoxDrives.Size = new System.Drawing.Size(119, 21);
+            this.comboBoxDrives.TabIndex = 0;
+            this.comboBoxDrives.SelectedIndexChanged += new System.EventHandler(this.comboBoxDrives_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(20, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "File explorer";
+            // 
+            // listBoxDirectories
+            // 
+            this.listBoxDirectories.FormattingEnabled = true;
+            this.listBoxDirectories.Location = new System.Drawing.Point(159, 105);
+            this.listBoxDirectories.Margin = new System.Windows.Forms.Padding(2);
+            this.listBoxDirectories.Name = "listBoxDirectories";
+            this.listBoxDirectories.Size = new System.Drawing.Size(217, 225);
+            this.listBoxDirectories.TabIndex = 2;
+            this.listBoxDirectories.SelectedIndexChanged += new System.EventHandler(this.listBoxDirectories_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(156, 86);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Directories";
+            // 
+            // listBoxFiles
+            // 
+            this.listBoxFiles.FormattingEnabled = true;
+            this.listBoxFiles.Location = new System.Drawing.Point(391, 105);
+            this.listBoxFiles.Margin = new System.Windows.Forms.Padding(2);
+            this.listBoxFiles.Name = "listBoxFiles";
+            this.listBoxFiles.Size = new System.Drawing.Size(226, 225);
+            this.listBoxFiles.TabIndex = 4;
+            this.listBoxFiles.SelectedIndexChanged += new System.EventHandler(this.listBoxFiles_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(374, 86);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Files";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(22, 86);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Choose directory";
+            // 
+            // textBoxPreview
+            // 
+            this.textBoxPreview.Location = new System.Drawing.Point(159, 347);
+            this.textBoxPreview.Multiline = true;
+            this.textBoxPreview.Name = "textBoxPreview";
+            this.textBoxPreview.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxPreview.Size = new System.Drawing.Size(457, 227);
+            this.textBoxPreview.TabIndex = 7;
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.BackColor = System.Drawing.Color.LemonChiffon;
+            this.textBoxPath.Location = new System.Drawing.Point(25, 42);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.ReadOnly = true;
+            this.textBoxPath.Size = new System.Drawing.Size(591, 20);
+            this.textBoxPath.TabIndex = 9;
+            // 
+            // buttonDirectoryUp
+            // 
+            this.buttonDirectoryUp.Font = new System.Drawing.Font("Wingdings", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.buttonDirectoryUp.Location = new System.Drawing.Point(255, 77);
+            this.buttonDirectoryUp.Name = "buttonDirectoryUp";
+            this.buttonDirectoryUp.Size = new System.Drawing.Size(30, 26);
+            this.buttonDirectoryUp.TabIndex = 10;
+            this.buttonDirectoryUp.Text = "ç";
+            this.buttonDirectoryUp.UseVisualStyleBackColor = true;
+            this.buttonDirectoryUp.Click += new System.EventHandler(this.buttonDirectoryUp_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 592);
+            this.Controls.Add(this.buttonDirectoryUp);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.textBoxPreview);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBoxFiles);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBoxDirectories);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxDrives);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "File explorer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxDrives;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBoxDirectories;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBoxFiles;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxPreview;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.Button buttonDirectoryUp;
+    }
+}
+
---
+++ ListDirectory/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListDirectory
+{
+
+    public partial class Form1 : Form
+    {
+        string drivePath;
+        string currentPath;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DriveInfo[] di = DriveInfo.GetDrives();
+
+            foreach (DriveInfo itemDrive in di)
+            {
+                comboBoxDrives.Items.Add(itemDrive.Name);
+            }
+        }
+
+        private void comboBoxDrives_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBoxDirectories.Items.Clear();
+            listBoxFiles.Items.Clear();
+
+            try
+            {
+                drivePath = comboBoxDrives.SelectedItem.ToString(); //TESTKOD
+
+                listBoxDirectories.Items.Clear();
+                //DriveInfo di = new DriveInfo(comboBoxDrives.SelectedItem.ToString());
+                DriveInfo di = new DriveInfo(drivePath);
+                // DirectoryInfo root = new DirectoryInfo(comboBoxDrives.SelectedItem.ToString());
+                DirectoryInfo root = new DirectoryInfo(drivePath);
+                DirectoryInfo[] dir = root.GetDirectories();
+                FileInfo[] files = root.GetFiles();
+
+                currentPath = drivePath;
+
+
+                foreach (DirectoryInfo d in dir)
+                {
+                    listBoxDirectories.Items.Add(d.Name);
+                }
+
+                foreach (FileInfo fi in files)
+                {
+                    listBoxFiles.Items.Add(fi.Name);
+                }
+
+                MessageBox.Show("Available Free Space: " + di.AvailableFreeSpace + "\n" +
+                    "Drive Format: " + di.DriveFormat + "\n" +
+                    "Drive Type: " + di.DriveType + "\n" +
+                    "Is Ready: " + di.IsReady.ToString() + "\n" +
+                    "Name: " + di.Name + "\n" +
+                    "Root Directory: " + di.RootDirectory + "\n" +
+                    "Total Free Space: " + di.TotalFreeSpace + "\n" +
+                    "Total Size: " + di.TotalSize + "\n" +
+                    "Volume Label: " + di.VolumeLabel.ToString(), di.Name +
+                    " DRIVE INFORMATION");
+
+                ShowCurrentPath();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void listBoxDirectories_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+
+                    listBoxFiles.Items.Clear(); //clears the files listbox
+
+                    DirectoryInfo root = new DirectoryInfo(currentPath);
+                    DirectoryInfo[] dir = root.GetDirectories();
+                    FileInfo[] files = root.GetFiles();
+
+                if (listBoxDirectories.SelectedIndex >= 0)
+                {
+                    int index = listBoxDirectories.SelectedIndex;
+                    currentPath = dir[index].FullName;
+                    listBoxDirectories.Items.Clear();
+
+                    root = new DirectoryInfo(currentPath);
+                    dir = root.GetDirectories();
+                    files = root.GetFiles();
+                }
+
+
+
+                    //listBoxDirectories.Items.Add(".."); //Sabbar hela systemet med index
+                    //currentPath = Directory.GetParent(currentPath).ToString();
+
+
+                    foreach (DirectoryInfo d in dir)
+                    {
+                        listBoxDirectories.Items.Add(d.Name);
+                    }
+
+
+                    foreach (FileInfo fi in files)
+                    {
+                        listBoxFiles.Items.Add(fi.Name);
+                    }
+
+                    ShowCurrentPath();
+               
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void listBoxFiles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+
+                DirectoryInfo root = new DirectoryInfo(currentPath);
+                FileInfo[] files = root.GetFiles();
+
+                int index = listBoxFiles.SelectedIndex; 
+                string filePath = files[index].FullName;
+
+                listBoxFiles.Items.Clear();
+
+                foreach (FileInfo fi in files)
+                {
+                    listBoxFiles.Items.Add(fi.Name);
+                }
+
+                ShowCurrentPath();
+
+
+                FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read); //innan merTest
+                StreamReader sr = new StreamReader(fs);
+                String buff = sr.ReadToEnd();
+                textBoxPreview.Text = buff;
+                //MessageBox.Show(buff);
+                sr.Close();
+                fs.Close();
+
+
+            }
+            catch
+            {
+                MessageBox.Show("Not a file.");
+            }
+
+        }
+
+        public void ShowCurrentPath()
+        {
+            textBoxPath.Text = currentPath;
+
+        }
+
+        private void buttonDirectoryUp_Click(object sender, EventArgs e)
+        {
+            if (currentPath != drivePath)
+            {
+                currentPath = Directory.GetParent(currentPath).ToString();
+                listBoxDirectories.Items.Clear();
+                listBoxDirectories_SelectedIndexChanged(sender, e);
+            }
+            else
+            {
+                MessageBox.Show("You are on the top level");
+            }
+        }
+    }
+}
+
